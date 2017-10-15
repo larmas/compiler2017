e38246e99c7714f6ea999a81620b0abf62755be6
larmas <lucas.armas.7@gmail.com>
2017-10-15 10:04:27 -0300

    Add text color on the scripts

--- script/runTestSintactico.sh
+++ script/runTestSintactico.sh
@@ -5,6 +5,13 @@
 # Usage: Situado en la raiz del proyecto correr el comando..
 #  $ ./script/runTestSintactico.sh
 
+RESET='\033[0m'
+BOLD='\033[1m'
+ITALIC='\033[3m'
+UNDERLINE='\033[4m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
 cd src/
 
 DIR=../test/test_sintactico
@@ -13,6 +20,8 @@ SRC=src/
 for i in $(ls $DIR)
 do
     printf "\n"
-    echo "In file: '"$i"'"
+    echo -e "${GREEN}${BOLD}In file: ${RESET}${GREEN}'"$i"'${RESET}"
+    printf "${RED}"
     ./run.out $DIR/$i
+    printf "${RESET}"
 done

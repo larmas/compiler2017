27748dc55191d3cefc3cc89ed8c0d428809e5039
larmas <lucas.armas.7@gmail.com>
2017-10-18 22:33:08 -0300

    Modified ctds.sh script for execute a particular test

---
+++ script/ctds.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+RESET='\033[0m'
+BOLD='\033[1m'
+ITALIC='\033[3m'
+UNDERLINE='\033[4m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+
+cd test/
+DIRSRC=../src
+DIRTEST=../test
+FLAG=false
+case $1 in
+    '-o')
+        if [ ! -f $2 ]; then
+            for i in $( ls )
+            do
+                if [ $i = $3 ]; then
+                    cd $DIRSRC
+                    FLAG=true
+                    printf  "\n\n${CYAN}-----COMPILE FLEX-----${RESET}"
+                    flex c-tds-lexico.l
+                    printf "\n\n${CYAN}-----COMPILE BISON-----${RESET}"
+                    bison -v -d c-tds-sintaxis.y
+                    printf "\n\n${CYAN}-----COMPILE GCC-----${RESET}\n"
+                    gcc -o $2 lex.yy.c c-tds-sintaxis.tab.c
+                    printf "\n\n${CYAN}-----EXECUTE-----${RESET}\n"
+                    ./$2 $DIRTEST/$3
+                fi
+            done
+            if [ $FLAG = false ]; then
+                printf "${RED} Invalid file: $3${RESET}"
+            fi
+        else
+            if [ -f $2 ]; then
+                printf "${RED}\n [ERROR] Debe proporcionar un identificador para el ejecutable.${RESET}\n"
+            fi
+        fi
+        ;;
+    '-target')
+        case $2 in
+            'scan')
+
+                ;;
+            'parse')
+
+                ;;
+            'codinter')
+
+                ;;
+            'assembly')
+
+                ;;
+        esac
+        ;;
+    '-opt')
+        ;;
+    *)
+        echo -e "${RED} Invalid argument $1${RESET}"
+        echo -e "${CYAN} Possible arguments: ${RESET}"
+        echo -e "${CYAN} -o <name>                    : for rename the executable <name>. ${RESET}"
+        echo -e "${CYAN} -target <stage>              : <stage> is a scan, parse, codinter, or assembly.\c"
+        echo -e "The compilation proceeds to the given stage. ${RESET}"
+        echo -e "${CYAN} -opt [list of optimizations] : Make the list of optimizations.\c"
+        echo -e "The all option performs all the supported optimizations. ${RESET}\n"
+        ;;
+    esac
